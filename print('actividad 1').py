955bc271579d022eacda1c037829f40305e6501f
Pitzo-code <diegoatj07@gmail.com>
2025-01-24 13:56:44 -0600

    actividad 1

---
+++ print('actividad 1').py
@@ -0,0 +1,43 @@
+#notasdel 24/01
+'''
+print('actividad 1')
+#and i hate every single thing in me
+
+nombre = input()
+print('tu nombre es;',nombre)
+
+#casting
+int()
+float()
+str()
+
+edad = input()
+anioactual =2025
+aniodenaciomiento = int(input('ingrese el año de nacimiento'))
+
+edad = anioactual - aniodenaciomiento
+
+print('tienes %d años'% edad )
+'''
+
+#actividad 1
+print('hola mundo')
+
+#actividad 2
+a = int(input('ingrese un numero: '))
+b = int(input('ingrese un numero: '))
+
+print('suma =', a + b)
+print('resta =', a - b)
+print('multiplicacion =', a * b)
+print('division =', a / b)
+'''
+if condicional:
+    comandos
+elif condicional 2:
+    'otrocomando'
+else
+    'comando final'
+
+#actividad 3
+'''

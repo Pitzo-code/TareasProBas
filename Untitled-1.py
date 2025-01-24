f1682fd76dbf2bccb1fe66f184c8d4669b3da318
Pitzo-code <diegoatj07@gmail.com>
2025-01-24 09:42:10 -0600

    trate de configurar todo

---
+++ Untitled-1.py
@@ -0,0 +1,14 @@
+#rint "holamundo"
+
+print(type("hola mundo"))
+
+v1 = 1 #entero
+v2 = 1.0 #florante
+v3 = "A" #texto
+print(v1,type(v1))
+v1= "holamundo"
+print (type("1989"))
+# pagin 34 "pyto para todos"
+v2 = "1989"
+print(v2)
+print('hola mundo')
